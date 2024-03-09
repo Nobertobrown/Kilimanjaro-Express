cee29684a229d5c701cef57e2c46bddc63910ca6
Nobertobrown <kibonanorberth@gmail.com>
2024-03-09 16:40:46 +0300

    [feat]: fix fetching routes for specific bus

--- controllers/route/fetchRoutes.js
+++ controllers/route/fetchRoutes.js
@@ -8,8 +8,7 @@ const fetchRoutes = async (req, res, next) => {
     const skip = (page - 1) * perPage;
 
     let mainQuery = {};
-
-    // Exclude 'page' from the main query
+    // Exclude 'page','amenities' & 'categories' from the main query
     if (Object.keys(req.query).length >= 1) {
       for (const key in req.query) {
         if (key !== "page" && key !== "amenities" && key !== "categories") {
@@ -18,26 +17,28 @@ const fetchRoutes = async (req, res, next) => {
       }
     }
 
-    let busQuery = {};
-    if (req.query.amenities) {
-      busQuery.amenities = { $in: req.query.amenities };
+    if (req.query.amenities || req.query.categories) {
+      let busQuery = {};
+      if (req.query.amenities) {
+        busQuery.amenities = { $in: req.query.amenities };
+      }
+      if (req.query.categories) {
+        busQuery.categories = { $in: req.query.categories };
+      }
+
+      const buses = await Bus.find(busQuery);
+      const busIds = buses.map((bus) => bus._id);
+
+      // If no buses match the criteria, return empty routes
+      if (busIds.length === 0) {
+        return res
+          .status(404)
+          .json({ success: false, error: "No routes found!" });
+      }
+
+      mainQuery.bus = { $in: busIds };
     }
-    if (req.query.categories) {
-      busQuery.categories = { $in: req.query.categories };
-    }
-
-    const buses = await Bus.find(busQuery);
-    const busIds = buses.map((bus) => bus._id);
-
-    // If no buses match the criteria, return empty routes
-    if (busIds.length === 0) {
-      return res
-        .status(404)
-        .json({ success: false, error: "No routes found!" });
-    }
-
-    mainQuery.bus = { $in: busIds };
-
+    
     const routes = await Route.find(mainQuery)
       .populate("bus", "name amenities categories -_id") // Populate required fields
       .skip(skip)
